11caaa9186229ab7e3ac607f06d18dd710f3c9ba
Ben Klosky <bk2988a@american.edu>
2024-07-29 18:43:23 +0000

    got data visualization working. I now need to replace dummy data vis with Shuman data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYmVua2xvc2t5IiwiYSI6ImNsejVwOTdnNzBxNjkyaXE1c
 
 function App() {
   // TODO: put below into appropriate separate file
-  // and import, rather than write inside App(), to prevent monolith
+  // and import, rather than write inside App(), to prevent monolith, just dont wanna debug rn
   //
   // these are the boilerplate params provided by Mapbox-gl
   const mapContainer = useRef(null);
@@ -29,12 +29,30 @@ function App() {
       center: [lng, lat],  // uses params above
       zoom: zoom  // zoom
     });
+    map.current.on('load', () => {
+      map.current.addSource('earthquakes', {
+        type: 'geojson',
+        // Use a URL for the value for the data property.
+        data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson'
+        });
+      map.current.addLayer({
+        'id': 'earthquakes-layer',
+        'type': 'circle',
+        'source': 'earthquakes',
+        'paint': {
+            'circle-radius': 4,
+            'circle-stroke-width': 2,
+            'circle-color': 'red',
+            'circle-stroke-color': 'white'
+        }
+      });
+    });
   });
 
   // this is what will actually be rendered by React's DOM
   return (
     <div className="App">
-      <h1 class=" text-3xl underline"> Public Transit Fairness Platform
+      <h1 className="font-serif text-3xl underline"> Public Transit Fairness Platform
       </h1>
       <div>
         <div ref={mapContainer} className="map-container" />
