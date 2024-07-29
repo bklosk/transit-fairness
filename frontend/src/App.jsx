a1dc93ec8f93b505b52e24d5c22ee6220cc7f950
Ben Klosky <bk2988a@american.edu>
2024-07-29 03:32:41 +0000

    addin' todos'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,25 @@ import { createRoot } from "react-dom/client";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import './style.css'
+import './style.css' //TODO: implement Tailwind so I can make stuff pretty
 
 // THIS IS THE DEFAULT PUBLIC TOKEN
-// I may be dumb, but I'm not stupid
-// TODO: replace with private environment variable securely read on server side
+// I may be dumb, but I'm not stupid   -ben
+// TODO: replace with secure read method
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmVua2xvc2t5IiwiYSI6ImNsejVwOTdnNzBxNjkyaXE1cDh0dDQzbzgifQ.3N3KSfATURB0JbJvwiEFmw';
 
 function App() {
-  // boilerplate params provided by Mapbox for a React map
+  // TODO: put below into appropriate separate file
+  // and import, rather than write inside App(), to prevent monolith
+  //
+  // these are the boilerplate params provided by Mapbox-gl
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
 
-  // function that initializes the map itself
+  // Effect hook (side effects in function components) that initializes the map
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -31,7 +34,7 @@ function App() {
   // this is what will actually be rendered by React's DOM
   return (
     <div className="App">
-      <h1>YO WHADDUP this a websiteee
+      <h1> Public Transit Fairness Platform
       </h1>
       <div>
         <div ref={mapContainer} className="map-container" />
